208c3ded42219731cec00a8a57b2f46c703a78b9
Paul Reesman <prrpar@msn.com>
2014-09-23 23:37:26 -0700

    Added fixes to Database connection; Added fetchone statements; Login page done; Create user page done; Added popups; Added database commit;

--- ScandiFest/main/BoothTool.py
+++ ScandiFest/main/BoothTool.py
@@ -1,6 +1,6 @@
 '''
 Created on Sep 15, 2014
-Updated on Sep 22, 2014
+Updated on Sep 23, 2014
 
 @author: Paul Reesman
 '''
@@ -99,12 +99,88 @@ class titlePage(Frame):
 class loginPage(Frame):
     def __init__(self, root, gui):
         Frame.__init__(self, root)
+        self.gui = gui
+        self.root = root
+        
+        self.config(background="blue")
+        Label(self, text="BoothTool.py\nVersion A.01", background="blue", height=20, width=root.winfo_width()).pack(side=TOP)
+        Label(self, text="Username:", background="blue", font=("Times", "20")).pack()
+        text1 = Entry(self, font=("Times", "16"))
+        text1.pack()
+        Label(self, text="Password:", background="blue", font=("Times", "20")).pack()
+        text2 = Entry(self, show="*", font=("Times", "16"))
+        text2.pack()
+        Button(self, text="Login...", font=("Times", "20"), command=lambda: self.loginCommand(text1.get(), text2.get())).pack()
+    
+    def loginCommand(self, username, password):
+        if database.login(username, password):
+            self.gui.screenChange(welcomePage(self.root, self.gui))
+        else:
+            popup = Toplevel()
+            popup.title("Incorrect")
+            
+            windowWidth = popup.winfo_screenwidth()
+            windowHeight = popup.winfo_screenheight()
+            
+            width = .25 * windowWidth
+            height = .1 * windowHeight
+            x = (windowWidth - width) / 2
+            y = (windowHeight - height) / 2
+            
+            popup.geometry("%dx%d+%d+%d" % (width, height, x, y))
+            popup.update()
+            
+            Label(popup, text="Incorrect username or password!").pack()
+            Button(popup, text="Dismiss", command=popup.destroy).pack()
+
+
+
+class welcomePage(Frame):
+    def __init__(self, root, gui):
+        Frame.__init__(self, root)
 
 
 
 class newUserPage(Frame):
     def __init__(self, root, gui):
         Frame.__init__(self, root)
+        
+        self.config(background="blue")
+        Label(self, text="Create A New User", background="blue", height=20, width=root.winfo_width()).pack(side=TOP)
+        Label(self, text="First Name:", background="blue", font=("Times", "20")).pack()
+        text1 = Entry(self, font=("Times", "16"))
+        text1.pack()
+        Label(self, text="Last Name:", background="blue", font=("Times", "20")).pack()
+        text2 = Entry(self, font=("Times", "16"))
+        text2.pack()
+        Label(self, text="Username:", background="blue", font=("Times", "20")).pack()
+        text3 = Entry(self, font=("Times", "16"))
+        text3.pack()
+        Label(self, text="Password:", background="blue", font=("Times", "20")).pack()
+        text4 = Entry(self, show="*", font=("Times", "16"))
+        text4.pack()
+        Button(self, text="Create User", font=("Times", "20"), command=lambda: self.createUser(text3.get(), text4.get(), text1.get(), text2.get())).pack()
+    
+    def createUser(self, username, password, fname, lname):
+        if database.newUser(username, password, fname, lname):
+            self.gui.screenChange(welcomePage(self.root, self.gui))
+        else:
+            popup = Toplevel()
+            popup.title("Username Exists!")
+            
+            windowWidth = popup.winfo_screenwidth()
+            windowHeight = popup.winfo_screenheight()
+            
+            width = .25 * windowWidth
+            height = .1 * windowHeight
+            x = (windowWidth - width) / 2
+            y = (windowHeight - height) / 2
+            
+            popup.geometry("%dx%d+%d+%d" % (width, height, x, y))
+            popup.update()
+            
+            Label(popup, text="A user account with that username already exists!").pack()
+            Button(popup, text="Dismiss", command=popup.destroy).pack()
 
 
 
@@ -112,13 +188,8 @@ def main():
     splash()
     root = Tk()
     GUI(root)
-    root.protocol("WM_DELETE_WINDOW", close(root))
+    root.protocol("WM_DELETE_WINDOW", root.destroy)
     root.mainloop()
-
-
-def close(root):
-    database.close()
-    root.destroy
 
 
 
--- ScandiFest/main/Logic.py
+++ ScandiFest/main/Logic.py
@@ -1,6 +1,6 @@
 '''
 Created on Sep 15, 2014
-updated on Sep 22, 2014
+updated on Sep 23, 2014
 
 @author: Paul Reesman
 '''
@@ -11,29 +11,31 @@ class Logic():
     pieNames = []
     
     def __init__(self, DB, IP='local host'):
-        try:
-            self.conn = lite.connect(DB)
-            self.cursor = self.conn.cursor()
-            
-            #self.cursor.execute("SELECT name FROM Freezer")
-            #pieNames = self.cursor.fetchall()
-            for member in self.cursor.execute("SELECT name FROM Freezer"):
-                self.pieNames.append(member)
-            print self.pieNames
+        self.conn = lite.connect(DB)
+        self.conn.execute('pragma foreign_keys = on')
+        self.conn.commit()
+        self.cursor = self.conn.cursor()
         
-        except:
-            self.exit()
+        #self.cursor.execute("SELECT name FROM Freezer")
+        #pieNames = self.cursor.fetchall()
+        #for member in self.cursor.execute("SELECT name FROM Freezer"):
+        #    self.pieNames.append(member)
+        #print self.pieNames
     
     def login(self, username, password):
-        if self.cursor.execute("SELECT EXISTS(SELECT * FROM users WHERE username=?)", username):
-            if password == self.execute("SELECT password FROM users WHERE username=?", username):
+        self.cursor.execute("SELECT EXISTS(SELECT * FROM users WHERE username=?)", (username,))
+        if self.cursor.fetchone():
+            self.cursor.execute("SELECT password FROM users WHERE username=?", (username,))
+            if password == self.cursor.fetchone():
                 return True
         return False
     
-    def newUser(self, username, password):
-        if self.cursor.execute("SELECT EXISTS(SELECT * FROM users WHERE username=?)", username):
+    def newUser(self, username, password, fname, lname):
+        self.cursor.execute("SELECT EXISTS(SELECT * FROM users WHERE username=?)", (username,))
+        if self.cursor.fetchone():
             return False
-        self.cursor.execute("INSERT INTO users(username, password) VALUES(?, ?)", (username, password))
+        self.cursor.execute("INSERT INTO users(username, password, fname, lname) VALUES(?, ?, ?, ?)", (username, password, fname, lname))
+        self.conn.commit()
         return True
     
     def close(self):
