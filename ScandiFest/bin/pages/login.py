dd4d9babb929c2471c3f442bbeba3569949f5601
Paul Reesman <reesmanp@onid.oregonstate.edu>
2015-11-01 18:59:07 -0800

    Removed the 'background image' implementation of the login page. Login page looks better now because of it and the screenChange function now works with the transfer between the login page and the welcome page.

--- ScandiFest/bin/pages/login.py
+++ ScandiFest/bin/pages/login.py
@@ -6,7 +6,7 @@ Created on Wed Sep 30 20:27:44 2015
 """
 
 
-from Tkinter import Frame, Label, Entry, Button, Toplevel, BOTH, TOP
+from Tkinter import Frame, Label, Entry, Button, Toplevel, TOP
 from welcome import welcomePage
 
 
@@ -19,20 +19,20 @@ class loginPage(Frame):
         #self.config(background="blue")
         #background_image = "images/rosemaling.gif"
         #self.image = PhotoImage(file=background_image)
-        background_label = Label(self, background="gainsboro").pack(expand=1, fill=BOTH)
+        self.config(background="gainsboro")
         
-        Label(background_label, text="BoothTool.py\nVersion A.01", background="gainsboro", height=20, width=root.winfo_width()).pack(side=TOP)
-        Label(background_label, text="Username:", background="gainsboro", font=("Times", "20")).pack()
+        Label(self, text="BoothTool.py\nVersion A.01", background="gainsboro", height=20, width=root.winfo_width()).pack(side=TOP)
+        Label(self, text="Username:", background="gainsboro", font=("Times", "20")).pack()
         
-        text1 = Entry(background_label, font=("Times", "16"), background="gainsboro")
+        text1 = Entry(self, font=("Times", "16"), background="gainsboro")
         text1.pack()
         
-        Label(background_label, text="Password:",background="gainsboro", font=("Times", "20")).pack()
+        Label(self, text="Password:",background="gainsboro", font=("Times", "20")).pack()
         
-        text2 = Entry(background_label, show="*", font=("Times", "16"), background="gainsboro")
+        text2 = Entry(self, show="*", font=("Times", "16"), background="gainsboro")
         text2.pack()
         
-        Button(background_label, text="Login...", font=("Times", "20"), background="gainsboro", command=lambda: self.loginCommand(text1.get(), text2.get())).pack()
+        Button(self, text="Login...", font=("Times", "20"), background="gainsboro", command=lambda: self.loginCommand(text1.get(), text2.get())).pack()
     
     def loginCommand(self, username, password):
         if self.gui.btd.database.login(username, password):
